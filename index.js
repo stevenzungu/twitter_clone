fa0f2ad402025865ce585ea4ae014d2bcbd7cb9d
Mac <Mac@Macs-Air.itpro.tv>
2017-01-27 19:09:38 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var server = express();
+
+var port = 8080;
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(request, response){
+  response.sendFile('index.html', { root: __dirname + '/public/html'});
+});
+
+server.get('/signup', function(request, response){
+  response.sendFile('signup.html', { root: __dirname + '/public/html'});
+});
+
+server.get('/login', function(request, response){
+  response.sendFile('login.html', { root: __dirname + '/public/html'});
+});
+
+server.listen(port, function(){
+  console.log("Now listening on port...", port);
+});
